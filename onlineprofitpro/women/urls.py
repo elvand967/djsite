f8e16f7f4104349f007041545ea32c5581bb3b22
Vladimir Dubanevich <vladimir.dubanevich@gmail.com>
2023-06-24 06:17:42 +0300

    templates start

--- onlineprofitpro/women/urls.py
+++ onlineprofitpro/women/urls.py
@@ -5,7 +5,8 @@ from .views import * # импортируем все текущие предст
 
 urlpatterns = [
     path('', index, name='home'), # http://127.0.0.1:8000/  # http://127.0.0.1:8000/women/
-    path('cats/<int:catid>/', categories), # http://127.0.0.1:8000/cats/1/  # http://127.0.0.1:8000/women/cats/
-    # path('cats/<slug:cat>/', categories), # http://127.0.0.1:8000/cats/test/
-    re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
+    path('about/', about, name='about'),
+    # path('cats/<int:catid>/', categories), # http://127.0.0.1:8000/cats/1/  # http://127.0.0.1:8000/women/cats/
+    # # path('cats/<slug:cat>/', categories), # http://127.0.0.1:8000/cats/test/
+    # re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
 ]
