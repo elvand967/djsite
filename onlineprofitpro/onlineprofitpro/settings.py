398297445eba4beef89396dadc5ecb9de08545e9
Vladimir Dubanevich <vladimir.dubanevich@gmail.com>
2023-06-24 06:55:26 +0300

    templates first step

--- onlineprofitpro/onlineprofitpro/settings.py
+++ onlineprofitpro/onlineprofitpro/settings.py
@@ -116,7 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'                           # префикс который будет добавлятся к названию статических файлов
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')    # путь к общей папке статических файлов в режиме эксплуатации
+STATICFILES_DIR = []                              # список нестандартных путей расположения статикфайлов
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
