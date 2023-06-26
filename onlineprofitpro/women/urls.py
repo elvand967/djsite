50e2f0cfeb41d18464018d4c3cdadb9bcf538e30
Vladimir Dubanevich <vladimir.dubanevich@gmail.com>
2023-06-26 08:55:53 +0300

    templates 2 step

--- onlineprofitpro/women/urls.py
+++ onlineprofitpro/women/urls.py
@@ -4,9 +4,11 @@ from django.urls import path, re_path
 from .views import * # импортируем все текущие представления текущего приложения
 
 urlpatterns = [
-    path('', index, name='home'), # http://127.0.0.1:8000/  # http://127.0.0.1:8000/women/
+    path('', index, name='home'),
     path('about/', about, name='about'),
-    # path('cats/<int:catid>/', categories), # http://127.0.0.1:8000/cats/1/  # http://127.0.0.1:8000/women/cats/
-    # # path('cats/<slug:cat>/', categories), # http://127.0.0.1:8000/cats/test/
-    # re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
+    path('addpage/', addpage, name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('post/<int:post_id>/', show_post, name='post'),
+    path('category/<int:cat_id>/', show_category, name='category'),
 ]
--- onlineprofitpro/women/views.py
+++ onlineprofitpro/women/views.py
@@ -6,32 +6,65 @@ from .models import *
 
 # Create your views here.
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+]
+
 
 def index(request):
-    #return HttpResponse("Страница приложения women.")
     posts = Women.objects.all()
-    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Главная страница'
+    }
+
+    return render(request, 'women/index.html', context=context)
+
+
+# def index(request):
+#     #return HttpResponse("Страница приложения women.")
+#     posts = Women.objects.all()
+#     return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
+
+# def categories(request, catid):
+#     if request.GET:
+#         print(request.GET)
+#     if request.POST:
+#         print(request.POST)
+#     return HttpResponse(f"<h1>Статьи по категориям.</h1><p>{catid}</p>")
+#
+#
+# def archive(request, year):
+#     if int(year)>2023:
+#         #raise Http404()
+#         #return redirect('/') # 302 - страница перемещена временно на другой URL-адрес
+#         return redirect('home', permanent=False) # 301 - страница перемещена на другой постоянный URL-адрес
+#
+#     return HttpResponse(f"<h1>Архив по годам.</h1><p>{year}</p>")
 
 def about(request):
     return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
 
-def categories(request, catid):
-    if request.GET:
-        print(request.GET)
-    if request.POST:
-        print(request.POST)
-    return HttpResponse(f"<h1>Статьи по категориям.</h1><p>{catid}</p>")
+
+def addpage(request):
+    return HttpResponse("Добавление статьи")
 
 
-def archive(request, year):
-    if int(year)>2023:
-        #raise Http404()
-        #return redirect('/') # 302 - страница перемещена временно на другой URL-адрес
-        return redirect('home', permanent=False) # 301 - страница перемещена на другой постоянный URL-адрес
+def contact(request):
+    return HttpResponse("Обратная связь")
 
-    return HttpResponse(f"<h1>Архив по годам.</h1><p>{year}</p>")
 
+def login(request):
+    return HttpResponse("Авторизация")
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
+
+def show_category(request, cat_id):
+    return HttpResponse(f"Отображение категории с id = {cat_id}")
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена.</h1>")
