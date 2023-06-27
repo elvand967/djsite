b3829d7bcc883f6fb0a311b5f5b871dfe8ebf7bc
Vladimir Dubanevich <vladimir.dubanevich@gmail.com>
2023-06-27 17:41:48 +0300

    templates foto

--- onlineprofitpro/women/models.py
+++ onlineprofitpro/women/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.urls import reverse
 
 class Women(models.Model):
-    title = models.CharField(max_length=255)  # заголовок (тип — строковый, длина — 255 символов). Поле, обязательное к заполнению;
-    content = models.TextField(blank=True)  # сам текст (тип — memo) (blank=True - означает что поле может быть пустым);
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/") # параметр определяет в какой каталог будем загружать изображения
+    title = models.CharField(max_length=255, verbose_name="Заголовок")  # заголовок (тип — строковый, длина — 255 символов). Поле, обязательное к заполнению;
+    content = models.TextField(blank=True, verbose_name="Текст статьи")  # сам текст (тип — memo) (blank=True - означает что поле может быть пустым);
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото") # параметр определяет в какой каталог будем загружать изображения
     # для корректной работы нужно настроить костанты
-    time_create = models.DateTimeField(auto_now_add=True) # время создание новой записи (auto_now_add=True - после создания не изменяется)
-    time_update = models.DateTimeField(auto_now=True) # время обновления записи (auto_now=True - изменяется каждый раз после изменения)
-    is_published = models.BooleanField(default=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания") # время создание новой записи (auto_now_add=True - после создания не изменяется)
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения") # время обновления записи (auto_now=True - изменяется каждый раз после изменения)
+    is_published = models.BooleanField(default=True, verbose_name="Публикация")
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name="Категория")
 
     def __str__(self):
         return self.title
@@ -17,9 +17,17 @@ class Women(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
 
-
+    class Meta:
+        verbose_name = 'Известную женщину' # отображение в единственном числе
+        verbose_name_plural = 'Известные женщины' # отображение в множественном числе
+        ordering = ['-time_create', 'title'] # сортировка по обратной дате создания поста и названию
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name="Категория")
+
+    class Meta:
+        verbose_name = 'Категорию' # отображение в единственном числе
+        verbose_name_plural = 'Категории' # отображение в множественном числе
+        ordering = ['id'] # сортировка по id
 
     def __str__(self):
         return self.name
--- onlineprofitpro/women/views.py
+++ onlineprofitpro/women/views.py
@@ -66,8 +66,24 @@ def login(request):
 def show_post(request, post_id):
     return HttpResponse(f"Отображение статьи с id = {post_id}")
 
+# def show_category(request, cat_id):
+#     return HttpResponse(f"Отображение категории с id = {cat_id}")
+
 def show_category(request, cat_id):
-    return HttpResponse(f"Отображение категории с id = {cat_id}")
+    posts = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+    if len(posts) == 0:
+        raise Http404()
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Главная страница',
+        'cat_selected': cat_id,
+    }
+    return render(request, 'women/index.html', context=context)
+
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена.</h1>")
